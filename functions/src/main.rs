10c799ecabeb536664b76ab144fb521bd1e3f25d
sungjin96 <sungjin5891@gmail.com>
2022-02-10 15:00:45 +0900

    3.3 함수 동작원리

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
     println!("Hello, world!");
+
+    another_function(5, 6);
+
+    let y = {
+        let x = 3;
+        x + 1
+    };
+    println!("The value of y is: {}", y);
+    println!("The value of five is: {}", five());
+    println!("The value of plus_one is: {}", plus_one(5));
+}
+
+fn another_function(x: i32, y: i32) {
+    println!("The value of x is: {} in function", x);
+    println!("The value of y is: {} in function", y);
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
