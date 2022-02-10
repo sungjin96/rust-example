4838de62d38dd48dde234f9fee76085b1e14ea5c
sungjin96 <sungjin5891@gmail.com>
2022-02-10 13:21:13 +0900

    3.1 변수와 가변성

--- universal_programming_concepts/src/main.rs
+++ universal_programming_concepts/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    // is Immutable ( 변수 )
+    // 참고 - 변수지만 Immutable 함. 상수(const)는 말 그대로 상수, Immutable 변수는 변수의 기본 설정이 Immutable.
+    // 즉 상수랑은 다름.
+    // let x = 5;
+
+    // is Mutable ( 변수 )
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    let a = 5;
+    let a = a + 1;
+    let a = a * 2;
+    println!("The value of a is: {}", a);
+
+    let spaces = "    ";
+    let spaces = spaces.len();
 }
