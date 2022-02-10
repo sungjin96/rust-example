d88fb82ee099d15231b6057ef88198c115e68f32
sungjin96 <sungjin5891@gmail.com>
2022-02-10 16:55:04 +0900

    3.5 제어문:

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
+    let mut number = 3;
+    while number != 0 {
+        println!("{}!", number);
+        number = number - 1;
+    }
+    println!("LIFTOFF");
 
+    let a = [10, 20, 30, 40,  50];
+    for element in a.iter() {
+        println!("the value is: {}", element)
+    }
+
+    for element in a.rev() {
+        println!("the reverse value is: {}", element)
+    }
 }
