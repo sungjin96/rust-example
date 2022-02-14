82f8bacfb1c028c8a468131c961cdd55a9158ac3
sungjin96 <sungjin5891@gmail.com>
2022-02-14 11:02:08 +0900

    5.2 구조체를 이용한 예제 프로그램

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,3 +1,19 @@
+#[derive(Debug)]
+struct Rectangle {
+    length: u32,
+    width: u32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let rect1 = Rectangle { length: 50, width: 30 };
+
+    println!("rect1 is {:#?}", rect1);
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(&rect1)
+    );
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.length * rectangle.width
 }
