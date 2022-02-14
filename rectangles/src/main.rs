2ad85587dc35e285b338a06b09090ebb89dfb14a
sungjin96 <sungjin5891@gmail.com>
2022-02-14 14:33:54 +0900

    5.3 메소드 문법

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -4,16 +4,26 @@ struct Rectangle {
     width: u32,
 }
 
-fn main() {
-    let rect1 = Rectangle { length: 50, width: 30 };
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.length * self.width
+    }
 
-    println!("rect1 is {:#?}", rect1);
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        area(&rect1)
-    );
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.length > other.length && self.width > other.width
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle { length: size, width: size };
+    }
 }
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.length * rectangle.width
+fn main() {
+    let rect1 = Rectangle { length: 50, width: 30 };
+    let _rect2 = Rectangle::square(10);
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
 }
