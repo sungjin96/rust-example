2bf8902b5c33a9411aaece29585488ce27dcfb39
sungjin96 <sungjin5891@gmail.com>
2022-02-15 13:44:35 +0900

    6.1 열거형 정의하기

--- enumerate/src/main.rs
+++ enumerate/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
 }
+
+impl Message {
+    fn call(&self) {
+        // 메소드 내용은 여기 정의할 수 있습니다.
+    }
+}
+
+
+enum Option<T> {
+    Some(T),
+    None,
+}
+
+fn main() {
+    let m = Message::Write(String::from("hello"));
+    m.call();
+
+    let _some_number = Some(5);
+    let _some_string = Some("a string");
+
+    let _absent_number: Option<i32> = None;
+}
+
