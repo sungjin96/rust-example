ed32b79223ee0febcc4c8f7a0ba6f91336ee60c2
sungjin96 <sungjin5891@gmail.com>
2022-02-16 16:31:45 +0900

    6.2 match 흐름 제어 연산자

--- enumerate/src/main.rs
+++ enumerate/src/main.rs
@@ -1,29 +1,43 @@
-enum Message {
-    Quit,
-    Move { x: i32, y: i32 },
-    Write(String),
-    ChangeColor(i32, i32, i32),
+#[derive(Debug)] // So we can inspect the state in a minute
+enum UsState {
+    Alabama,
+    Alaska,
+    // ... etc
 }
 
-impl Message {
-    fn call(&self) {
-        // 메소드 내용은 여기 정의할 수 있습니다.
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
     }
 }
 
-
-enum Option<T> {
-    Some(T),
-    None,
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        },
+        _ => (),
+    }
 }
 
 fn main() {
-    let m = Message::Write(String::from("hello"));
-    m.call();
+    let value = value_in_cents(Coin::Dime);
+    println!("{}", value);
 
-    let _some_number = Some(5);
-    let _some_string = Some("a string");
-
-    let _absent_number: Option<i32> = None;
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
 }
 
