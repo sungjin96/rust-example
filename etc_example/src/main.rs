41fd89187fa65b820eb8838af437ea9413c722bd
sungjin96 <sungjin5891@gmail.com>
2022-02-16 19:42:20 +0900

    Fancy printing

--- etc_example/src/main.rs
+++ etc_example/src/main.rs
@@ -1,6 +1,21 @@
 fn main() {
-    let my_number = 10;
-    let single_refernce = &my_number;
-    let double_reference = &single_refernce;
-    let five_reference = &&&&&my_number;
+    print!(r#"c:\thisdrice\new_drive"#);
+    println!("
+let me tell you
+어떤 이야기를
+봅시다.
+             ");
+
+    let my_variable = 9;
+    // p = pointer, b = 2byte 등등..
+    println!("{:p}", &my_variable);
+
+
+    let title = "TODAY'S NEWS";
+    println!("{:-^30}", title);
+    let bar = "|";
+    println!("{: <15}{: >15}", bar, bar);
+    let a = "SEOUL";
+    let b = "TOKYO";
+    println!("{city1:-<15}{city2:->15}", city1 = a, city2 = b);
 }
