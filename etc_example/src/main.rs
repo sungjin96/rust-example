9a1e57b4912bae835df17ebeb8068efdde328665
sungjin96 <sungjin5891@gmail.com>
2022-02-21 20:20:04 +0900

    String Function

--- etc_example/src/main.rs
+++ etc_example/src/main.rs
@@ -1,21 +1,6 @@
 fn main() {
-    print!(r#"c:\thisdrice\new_drive"#);
-    println!("
-let me tell you
-어떤 이야기를
-봅시다.
-             ");
-
-    let my_variable = 9;
-    // p = pointer, b = 2byte 등등..
-    println!("{:p}", &my_variable);
-
-
-    let title = "TODAY'S NEWS";
-    println!("{:-^30}", title);
-    let bar = "|";
-    println!("{: <15}{: >15}", bar, bar);
-    let a = "SEOUL";
-    let b = "TOKYO";
-    println!("{city1:-<15}{city2:->15}", city1 = a, city2 = b);
+    let mut my_name = "David".to_string();
+    my_name.push('!');
+    my_name.push_str(" and I live in Seoul");
+    println!("My name is {}", my_name);
 }
