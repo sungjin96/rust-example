8e8c16bb09bd276ba285a48b70e739f6b1fe2092
sungjin96 <sungjin5891@gmail.com>
2022-02-28 13:52:13 +0900

    array

--- etc_example/src/main.rs
+++ etc_example/src/main.rs
@@ -1,18 +1,8 @@
-fn loop_then_return(mut counter: i32) -> i32{
-   loop {
-       counter += 1;
-       if counter % 50 == 0 {
-           break;
-       }
-   }
-    counter
-}
+// Collection types
+// Array
 
 fn main() {
-    let my_number;
-    {
-        let x = loop_then_return(0);
-        my_number = x
-    };
-    println!("{}", my_number);
+    let array = [0; 640];
+    println!("{:?}", array);
+
 }
