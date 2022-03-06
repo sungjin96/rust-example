eca9390aa9d2415986e911c3c0f592fb35f9c571
sungjin96 <sungjin5891@gmail.com>
2022-03-06 13:59:45 +0900

    From and Info

--- etc_example/src/main.rs
+++ etc_example/src/main.rs
@@ -1,8 +1,10 @@
-// Collection types
-// Array
+// trait = 초능력
+// This type implements (trait name)
+
+// From, Into
 
 fn main() {
-    let array = [0; 640];
-    println!("{:?}", array);
-
+    let my_name = String::from("dave Macleod");
+    let my_city: String = "Seoul".into();
+    let my_vec = Vec::from([1, 2, 3]);
 }
