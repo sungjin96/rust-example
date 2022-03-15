35cc79eadd1bb486e4a1aeb48a531f2817551282
sungjin96 <sungjin5891@gmail.com>
2022-03-15 22:18:07 +0900

    struct size

--- etc_example/src/main.rs
+++ etc_example/src/main.rs
@@ -1,34 +1,35 @@
-// struct = and
-// enum = or
+use std::mem::size_of_val;
 
-//unit struct = 0 byte
-struct FileDirectory;
-
-// trait
-// tuple struct
-#[derive(Debug)] // attribute = println을 위한 것
-struct Color(u8, u8, u8);
-
-// named Struct
 #[derive(Debug)]
 struct Country {
     population: u32,
     capital: String,
-    leader_name: String
+    leader_name: String,
 }
 
+struct Numbers {
+    one: u8,
+    two: u8,
+    three: u8,
+    four: char
+}
+
+
 fn main() {
-    let x = FileDirectory;
-    println!("The size is {}", std::mem::size_of_val(&x));
-
-    let my_color = Color(20, 50, 100);
-    println!("The color is {:?}", my_color);
-
-    let canada = Country {
-        population: 35_000_000,
-        capital: "Ottawa".to_string(),
-        leader_name: "Justin Trudeau".to_string()
-    };
+    let population = 35_000_000;
+    let capital = "Ottawa".to_string();
+    let leader_name = "Justin Trudeau".to_string();
+    let canada = Country { population, capital, leader_name };
     println!("The Country is: {:#?}", canada);
     println!("The Country is: {:?}", canada.capital);
+    println!("Country is {} bytes in size", size_of_val(&canada));
+
+    let numbers = Numbers {
+        one: 8,
+        two: 19,
+        three: 20,
+        four: 'a'
+    };
+
+    println!("Numbers is {} bytes in size", size_of_val(&numbers));
 }
