0484bd47c8c600248ee5b57218d6abaa960a8bca
sungjin96 <sungjin5891@gmail.com>
2022-03-15 22:03:10 +0900

    struct

--- etc_example/src/main.rs
+++ etc_example/src/main.rs
@@ -1,10 +1,34 @@
-fn match_number(input: i32) {
-    match input {
-        number @ 0..=10 => println!("it's between 0 and 10. {}", number),
-        _ => println!("It's greater than ten")
-    }
+// struct = and
+// enum = or
+
+//unit struct = 0 byte
+struct FileDirectory;
+
+// trait
+// tuple struct
+#[derive(Debug)] // attribute = println을 위한 것
+struct Color(u8, u8, u8);
+
+// named Struct
+#[derive(Debug)]
+struct Country {
+    population: u32,
+    capital: String,
+    leader_name: String
 }
 
 fn main() {
+    let x = FileDirectory;
+    println!("The size is {}", std::mem::size_of_val(&x));
 
+    let my_color = Color(20, 50, 100);
+    println!("The color is {:?}", my_color);
+
+    let canada = Country {
+        population: 35_000_000,
+        capital: "Ottawa".to_string(),
+        leader_name: "Justin Trudeau".to_string()
+    };
+    println!("The Country is: {:?}", canada);
+    println!("The Country is: {:?}", canada.capital);
 }
