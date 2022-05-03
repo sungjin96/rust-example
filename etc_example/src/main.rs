bc8564cbfe8a4c0d6e82df1860b9246366f7103f
sungjin96 <sungjin5891@gmail.com>
2022-05-03 16:26:05 +0900

    ep 16

--- etc_example/src/main.rs
+++ etc_example/src/main.rs
@@ -1,10 +1,30 @@
 fn main() {
-    string_method();
+    const_and_static();
 }
+// ==============================
+
+
+// 16 강
+// const
+// static
+//
+// 1. Type declaration required
+// 2. Only Uppercase name ( ex - NUMBER, HIGH_SCORE ... )
+const HIGH_SCORE: i32 = 10;
+static LOW_SCORE: i32 = 0; // unsafe 로 수정은 가능하지만 다른 방법을 고민 해야함.
+
+fn print_high_score() {
+   println!("The high score is {HIGH_SCORE}") ;
+}
+
+fn const_and_static() {
+   print_high_score();
+}
+// ==============================
 
 // 15 강
 // capacity / push / push_str / pop / with_capacity
-fn string_method() {
+fn _string_method() {
     let mut my_name = String::with_capacity(4);
     println!("{}", my_name.capacity());
     my_name.push_str("SJ");
@@ -12,6 +32,7 @@ fn string_method() {
     my_name.push('!');
     println!("My name is {}", my_name);
 }
+// ==============================
 
 // 14 강
 fn _string_and_str() {
@@ -24,3 +45,4 @@ fn _string_and_str() {
     my_other_name.push('!');
     println!("{my_other_name}");
 }
+// ==============================
