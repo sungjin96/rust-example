6c13a67106d48d0f57eff78b0069a375dfdf5f18
sungjin96 <sungjin5891@gmail.com>
2022-05-03 16:13:44 +0900

    ep15

--- etc_example/src/main.rs
+++ etc_example/src/main.rs
@@ -1,9 +1,20 @@
 fn main() {
-    string_and_str();
+    string_method();
+}
+
+// 15 강
+// capacity / push / push_str / pop / with_capacity
+fn string_method() {
+    let mut my_name = String::with_capacity(4);
+    println!("{}", my_name.capacity());
+    my_name.push_str("SJ");
+    println!("{}", my_name.capacity());
+    my_name.push('!');
+    println!("My name is {}", my_name);
 }
 
 // 14 강
-fn string_and_str() {
+fn _string_and_str() {
     // String = Sized type -> heap
     // &str = Dynamic type
     let _my_name = "SJ"; // &str
