be16bbcdecc7b6aa7a1a7a5ebdaa131d4c5fad0b
sungjin58 <sungjin@payme.co.kr>
2022-05-06 10:15:47 +0900

    Changes

---
+++ etc_example/src/bin/from_and_into.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let my_name = String::from("Dave MacLed");
+    let my_city: String = "Seoul".into();
+    let my_vec = Vec::from([8, 9, 10]);
+
+    println!("{}", my_city);
+    println!("{:?}", my_vec);
+}
