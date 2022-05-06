c90b39e2ac4cfa169323e17973f1640bb10c741b
sungjin58 <sungjin@payme.co.kr>
2022-05-06 09:40:35 +0900

    ep 24

---
+++ etc_example/src/bin/array.rs
@@ -0,0 +1,12 @@
+// Collection types
+// Array
+
+// Option Enum - ( Some, None )
+
+fn main() {
+    let _num_array = [8, 9, 10, 22];
+    let string_array = ["One", "Two"];
+    let _zero_array = [0; 100]; // [ 0, 0, 0, 0, 0, ......, 0 ];
+
+    println!("{:?}", string_array.get(1));
+}
