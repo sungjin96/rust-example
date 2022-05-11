8ade52fd3781853a32819629aa1b8b192d17cfaa
sungjin96 <sungjin5891@gmail.com>
2022-05-11 21:30:28 +0900

    ap 38

---
+++ etc_example/src/bin/loops.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let mut counter = 0;
+    let mut counter2 = 0;
+
+    'first_loop: loop {
+        counter += 1;
+        println!("The counter is now: {counter}");
+        if counter > 9 {
+            println!("Now entering the second loop");
+
+            'second_loop: loop {
+               println!("THe second counter is: {counter2}")     ;
+                counter2 += 1;
+                if counter2 == 3 {
+                    break 'first_loop;
+                }
+            }
+        }
+    }
+}
