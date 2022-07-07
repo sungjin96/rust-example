d6b0820de3c415a43aa66462dbc0417517a43df0
sungjin96 <sungjin5891@gmail.com>
2022-07-07 20:19:53 +0900

    Web Assembly Test

--- hello-wasm/site/index.js
+++ hello-wasm/site/index.js
@@ -1,4 +1,4 @@
-const js = import("./node_modules/@sungjin5891/hello-wasm/hello_wasm.js");
-js.then(js => {
-    js.greet("WebAssembly");
-});
+const js = import("@sungjin5891/hello-wasm/hello_wasm.js");
+js
+    .then(js => js.greet("WebAssembly"))
+    .catch(console.error)
