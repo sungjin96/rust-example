aa1148e7bdf62e8155847d53adc88d872791a360
sungjin96 <sungjin5891@gmail.com>
2022-10-21 20:08:49 +0900

    a7

--- easy-rust/src/bin/a7.rs
+++ easy-rust/src/bin/a7.rs
@@ -1,13 +1,31 @@
 // Topic: Working with an enum
 //
 // Program requirements:
-// * Prints the name of a color to the terminal
+// * Prints the name of a Color to the terminal
 //
 // Notes:
-// * Use an enum with color names as variants
-// * Use a function to print the color name
+// * Use an enum with Color names as variants
+// * Use a function to print the Color name
 // * The function must use the enum as a parameter
-// * Use a match expression to determine which color
+// * Use a match expression to determine which Color
 //   name to print
 
-fn main() {}
+enum Color {
+    Red,
+    Yellow,
+    Blue
+}
+
+fn print_color(color: Color) {
+
+    match color {
+        Color::Red => println!("the red"),
+        Color::Yellow => println!("the yellow"),
+        Color::Blue => println!("the Blue"),
+    }
+}
+
+fn main() {
+    let color = Color::Blue;
+    print_color(color);
+}
