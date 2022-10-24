84ea286a41f015009f9fef95caf6c76ef1e2c955
sungjin96 <sungjin5891@gmail.com>
2022-10-24 21:14:31 +0900

    a12

--- easy-rust/src/bin/a12.rs
+++ easy-rust/src/bin/a12.rs
@@ -6,8 +6,35 @@
 //
 // Notes:
 // * Use a struct to encapsulate the box characteristics
+struct Box {
+    color: BoxColor,
+    weight: f32,
+    dimensions: f32,
+}
+
 // * Use an enum for the box color
+enum BoxColor {
+    RED,
+    BLUE,
+}
 // * Implement functionality on the box struct to create a new box
 // * Implement functionality on the box struct to print the characteristics
+impl Box {
+    fn create() -> Box {
+        Box {
+            color: BoxColor::RED,
+            weight: 1.0,
+            dimensions: 2.0
+        }
+    }
+    fn print_char(&self) {
+       println!("{:?}", self.dimensions);
+       println!("{:?}", self.weight);
+    }
+}
 
-fn main() {}
+
+fn main() {
+    let box_data = Box::create();
+    box_data.print_char();
+}
