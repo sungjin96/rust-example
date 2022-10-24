67e874397fde19d2425d3aa87acf4402d3156325
sungjin96 <sungjin5891@gmail.com>
2022-10-24 21:06:17 +0900

    a11

--- easy-rust/src/bin/a11.rs
+++ easy-rust/src/bin/a11.rs
@@ -6,7 +6,25 @@
 // Notes:
 // * Use a struct for the grocery item
 // * Use two i32 fields for the quantity and id number
+struct Grocery {
+    id: i32,
+    quantity: i32,
+}
 // * Create a function to display the quantity, with the struct as a parameter
+fn display_quantity(grocery: &Grocery) {
+   println!("{:?}", grocery.quantity);
+}
 // * Create a function to display the id number, with the struct as a parameter
+fn display_id(grocery: &Grocery) {
+    println!("{:?}", grocery.id);
+}
 
-fn main() {}
+fn main() {
+    let grocery = Grocery {
+        id: 1,
+        quantity: 10
+    };
+
+    display_quantity(&grocery);
+    display_id(&grocery);
+}
