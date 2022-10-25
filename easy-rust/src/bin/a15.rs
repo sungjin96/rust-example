6ad597a0bab316358fb40c87fc0d54c950e29732
sungjin96 <sungjin5891@gmail.com>
2022-10-25 19:32:34 +0900

    a15

--- easy-rust/src/bin/a15.rs
+++ easy-rust/src/bin/a15.rs
@@ -11,4 +11,24 @@
 // * Create one of each ticket and place into a vector
 // * Use a match expression while iterating the vector to print the ticket info
 
-fn main() {}
+enum Ticket {
+    Backstage(f64, String),
+    Vip(f64, String),
+    Standard(f64),
+}
+
+fn main() {
+    let tickets = vec![
+        Ticket::Backstage(50.0, "Billy".to_owned()),
+        Ticket::Vip(30.0, String::from("Age")),
+        Ticket::Standard(15.0),
+    ];
+
+    for ticket in tickets {
+        match ticket {
+            Ticket::Backstage(price, holder) => println!("Holder: {:?}, price: {:?}", price, holder),
+            Ticket::Vip(.., holder) => println!("Holder: {:?}, price: {:?}", price, holder),
+            Ticket::Standard(price) => println!("price: {:?}", price)
+        }
+    }
+}
