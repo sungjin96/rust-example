fab94c750db95c56523d09122a7db220b1d6dd9c
sungjin96 <sungjin5891@gmail.com>
2022-10-25 19:13:38 +0900

    a14

--- easy-rust/src/bin/a14.rs
+++ easy-rust/src/bin/a14.rs
@@ -5,10 +5,34 @@
 //
 // Notes:
 // * Use a struct for a persons age, name, and favorite color
+struct Person {
+    name: String,
+    age: u8,
+    color: Color,
+}
+
+enum Color {
+    Red,
+    Blue,
+}
 // * The color and name should be stored as a String
 // * Create and store at least 3 people in a vector
 // * Iterate through the vector using a for..in loop
 // * Use an if expression to determine which person's info should be printed
 // * The name and colors should be printed using a function
 
-fn main() {}
+fn print(data: &str) {
+    println!("{:?}", data);
+}
+
+fn main() {
+    let peoples = vec![
+        Person { name: "hi".to_owned(), age: 1, color: Color::Blue }
+    ];
+
+    for person in peoples {
+        if person.age <= 10 {
+            print(&person.name);
+        }
+    }
+}
