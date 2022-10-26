a69f81150760f49548b6b230611f9d77809eb3c8
sungjin96 <sungjin5891@gmail.com>
2022-10-26 20:48:35 +0900

    a18b

--- easy-rust/src/bin/a18b.rs
+++ easy-rust/src/bin/a18b.rs
@@ -21,5 +21,54 @@
 //   may enter the building
 // * Print whether the employee may access the building
 //   * Must use a function that utilizes the question mark operator to do this
+enum Position {
+    Maintenance,
+    Marketing,
+    Managers,
+    LineSupervisor,
+    KitchenStaff,
+    AssemblyTech,
+}
 
-fn main() {}
+enum Status {
+    Active,
+    Terminated,
+}
+
+struct Employee {
+    position: Position,
+    status: Status,
+}
+
+fn try_access(employee: &Employee) -> Result<(), String> {
+    match employee.status {
+        Status::Terminated => return Err("terminated".to_owned()),
+        _ => (),
+    }
+
+    match employee.position {
+        Position::Maintenance => Ok(()),
+        Position::Marketing => Ok(()),
+        Position::Managers => Ok(()),
+        _ => Err("invalid position".to_owned())
+    }
+}
+
+fn print_access(employee: &Employee) -> Result<(), String> {
+    let attempt_access = try_access(employee)?;
+    println!("access ok");
+    Ok(())
+}
+
+fn main() {
+    let manager = Employee {
+        position: Position::AssemblyTech,
+        status: Status::Active
+    };
+
+    match print_access(&manager) {
+        Err(e) => println!("access denied: {:?}", e),
+        _ => ()
+    }
+
+}
