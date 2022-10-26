024fc69576f1f596b59240ef0373de87863272e9
sungjin96 <sungjin5891@gmail.com>
2022-10-26 20:39:43 +0900

    a18

--- easy-rust/src/bin/a18.rs
+++ easy-rust/src/bin/a18.rs
@@ -10,5 +10,20 @@
 // * Use a function to determine if a customer can make a restricted purchase
 // * Return a result from the function
 // * The Err variant should detail the reason why they cannot make a purchase
+struct Customer {
+    age: i32,
+}
 
-fn main() {}
+fn try_purchase(customer: &Customer) -> Result<(), String> {
+    if customer.age < 21 {
+        Err("customer must be at least 21 years old".to_owned())
+    } else {
+        Ok(())
+    }
+}
+
+fn main() {
+    let customer = Customer{ age: 12 };
+    let result = try_purchase(&customer);
+    println!("{:?}", result)
+}
