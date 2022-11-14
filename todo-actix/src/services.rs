b1be36eb3be109f8a02b567ce6ba1f004afa22ce
sungjin96 <sungjin5891@gmail.com>
2022-11-14 21:33:50 +0900

    actix example

--- todo-actix/src/services.rs
+++ todo-actix/src/services.rs
@@ -1,3 +1,4 @@
+use std::fmt::format;
 use crate::{AppState, TokenClaims};
 use actix_web::{
     get, post,
@@ -13,8 +14,43 @@ use serde::{Deserialize, Serialize};
 use sha2::Sha256;
 use sqlx::{self, FromRow};
 
+#[derive(Deserialize)]
+struct CreateUserBody {
+    username: String,
+    password: String,
+}
+
+#[derive(Serialize, FromRow)]
+struct UserNoPassword {
+    id: i32,
+    username: String,
+}
+
 #[post("/user")]
-pub async fn create_user(state: Data<AppState>) -> impl Responder {}
+pub async fn create_user(state: Data<AppState>, body: Json<CreateUserBody>) -> impl Responder {
+    let user: CreateUserBody = body.into_inner();
+
+    let hash_secret = std::env::var("HASH_SECRET").expect("HASH_SECRET must be set!");
+    let mut hasher = Hasher::default();
+    let hash = hasher
+        .with_password(user.password)
+        .with_secret_key(hash_secret)
+        .hash()
+        .unwrap();
+
+    match sqlx::query_as::<_, UserNoPassword>(
+        "INSERT INTO users (username, password)\
+        VALUE ($1, $2)\
+        RETURNING id, username"
+    )
+        .bind(user.username)
+        .bind(hash)
+        .fetch_one(&state.db)
+        .await {
+        Ok(user) => HttpResponse::Ok().json(user),
+        Err(error) => HttpResponse::InternalServerError().json(format!("{:?}", error))
+    }
+}
 
 #[get("/auth")]
 pub async fn basic_auth(state: Data<AppState>) -> impl Responder {}
