9f6f0dbca5f5f8b706b82c81ca90b8625e501e45
sungjin96 <sungjin5891@gmail.com>
2022-11-16 20:38:26 +0900

    /auth

--- todo-actix/src/services.rs
+++ todo-actix/src/services.rs
@@ -26,6 +26,13 @@ struct UserNoPassword {
     username: String,
 }
 
+#[derive(Serialize, FromRow)]
+struct AuthUser {
+    id: i32,
+    username: String,
+    password: String,
+}
+
 #[post("/user")]
 pub async fn create_user(state: Data<AppState>, body: Json<CreateUserBody>) -> impl Responder {
     let user: CreateUserBody = body.into_inner();
@@ -53,7 +60,48 @@ pub async fn create_user(state: Data<AppState>, body: Json<CreateUserBody>) -> i
 }
 
 #[get("/auth")]
-pub async fn basic_auth(state: Data<AppState>) -> impl Responder {}
+pub async fn basic_auth(state: Data<AppState>, credentials: BasicAuth) -> impl Responder {
+    let jwt_secret: Hmac<Sha256> = Hmac::new_from_slice(
+        std::env::var("JWT_SECRET")
+            .expect("JWT_SECRET must be set!")
+            .as_bytes(),
+    ).unwrap();
+    let username = credentials.user_id();
+    let password = credentials.password();
+
+    match password {
+        None => HttpResponse::Unauthorized().json("Must provide username and password"),
+        Some(pass) => {
+            match sqlx::query_as::<_, AuthUser>(
+                "SELECT id, username password FROM users WHERE username = $1",
+            )
+                .bind(username.to_owned())
+                .fetch_one(&state.db)
+                .await
+            {
+                Ok(user) => {
+                    let hash_secret = std::env::var("HASH_SECRET").expect("HASH_SECRET must be set!");
+                    let mut verifier = Verifier::default();
+                    let is_valid = verifier
+                        .with_hash(user.password)
+                        .with_password(pass)
+                        .with_secret_key(hash_secret)
+                        .verify()
+                        .unwrap();
+
+                    if is_valid {
+                        let claims = TokenClaims { id: user.id };
+                        let token_str = claims.sign_with_key(&jwt_secret).unwrap();
+                        HttpResponse::Ok().json(token_str)
+                    } else {
+                        HttpResponse::Unauthorized().json("Incorrect username or password")
+                    }
+                }
+                Err(error) => HttpResponse::InternalServerError().json(format!("{:}", error))
+            }
+        }
+    }
+}
 
 #[post("/article")]
 pub async fn create_article(state: Data<AppState>) -> impl Responder {}
