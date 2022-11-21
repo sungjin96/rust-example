b0944132f4576535580222f5b0c245a1e08b9935
sungjin96 <sungjin5891@gmail.com>
2022-11-21 21:53:15 +0900

    add mirror_custom_header api

--- axum-tutorial/routing/src/routes/mod.rs
+++ axum-tutorial/routing/src/routes/mod.rs
@@ -4,6 +4,7 @@ mod mirror_body_json;
 mod path_variables;
 mod query_params;
 mod mirror_user_agent;
+mod mirror_custom_header;
 
 use axum::{body::Body, routing::{get, post}, Router};
 use hello_world::hello_world;
@@ -12,6 +13,7 @@ use mirror_body_json::mirror_body_json;
 use path_variables::{path_variables, hard_coded_path};
 use query_params::query_params;
 use mirror_user_agent::mirror_user_agent;
+use mirror_custom_header::mirror_custom_header;
 
 pub fn create_routes() -> Router<Body> {
     Router::new()
@@ -22,4 +24,5 @@ pub fn create_routes() -> Router<Body> {
         .route("/path-variables/15", get(hard_coded_path))
         .route("/query-params", get(query_params))
         .route("/mirror-user-agent", get(mirror_user_agent))
+        .route("/mirror-custom-header", get(mirror_custom_header))
 }
